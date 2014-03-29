72c02f2bded65a7b1491dbd7839c6ecbc27bfa34
nitram509 <maki@bitkings.de>
2014-03-29 21:17:48 +0100

    pimped tweening example

--- animationszauber/js/slides/tweening-beispiel.js
+++ animationszauber/js/slides/tweening-beispiel.js
@@ -14,48 +14,73 @@
 
     var animModel = {
       x: 10,
-      y: 260,
-      minX : 10,
-      minY : 0,
-      maxX : 190,
-      maxY : 260
+      y: 280,
+      r: 0,
+
+      xFrom: 10,
+      yFrom: 20,
+      rFrom: 0,
+      xTo: 190,
+      yTo: 280,
+      rTo: 360
     };
 
     var tween1 = new TWEEN.Tween(animModel)
-        .to({ x: animModel.maxX }, duration)
-        .easing(TWEEN.Easing.Elastic.InOut)
+        .to({ x: animModel.xTo }, duration)
+        .easing(TWEEN.Easing.Quartic.InOut)
         .onUpdate(function () {
           bedconLogo.style.left = this.x + 'px';
         });
-    var tween2 = new TWEEN.Tween(animModel)
-        .to({ y: animModel.minY }, duration)
+    var tween1r = new TWEEN.Tween(animModel)
+        .to({ r: animModel.rTo }, duration)
         .easing(TWEEN.Easing.Quartic.InOut)
+        .onUpdate(function () {
+          bedconLogo.style['-webkit-transform'] = 'rotate(' + this.r + 'deg)';
+          bedconLogo.style['-moz-transform'] = 'rotate(' + this.r + 'deg)';
+          bedconLogo.style['transform'] = 'rotate(' + this.r + 'deg)';
+        });
+    var tween2 = new TWEEN.Tween(animModel)
+        .to({ y: animModel.yFrom }, duration)
+        .easing(TWEEN.Easing.Elastic.InOut)
         .onUpdate(function () {
           bedconLogo.style.bottom = this.y + 'px';
         });
     var tween3 = new TWEEN.Tween(animModel)
-        .to({ x: animModel.minX }, duration)
-        .easing(TWEEN.Easing.Elastic.InOut)
+        .to({ x: animModel.xFrom }, duration)
+        .easing(TWEEN.Easing.Quartic.InOut)
         .onUpdate(function () {
           bedconLogo.style.left = this.x + 'px';
         });
-    var tween4 = new TWEEN.Tween(animModel)
-        .to({ y: animModel.maxY }, duration)
+    var tween3r = new TWEEN.Tween(animModel)
+        .to({ r: animModel.rFrom }, duration)
         .easing(TWEEN.Easing.Quartic.InOut)
+        .onUpdate(function () {
+          bedconLogo.style['-webkit-transform'] = 'rotate(' + this.r + 'deg)';
+          bedconLogo.style['-moz-transform'] = 'rotate(' + this.r + 'deg)';
+          bedconLogo.style['transform'] = 'rotate(' + this.r + 'deg)';
+        })
+        /*.onComplete(function() {
+          animModel.r = animModel.rFrom;
+        })*/;
+    var tween4 = new TWEEN.Tween(animModel)
+        .to({ y: animModel.yTo }, duration)
+        .easing(TWEEN.Easing.Elastic.InOut)
         .onUpdate(function () {
           bedconLogo.style.bottom = this.y + 'px';
         });
 
     tween1.chain(tween2);
-    tween2.chain(tween3);
+    tween2.chain(tween3, tween3r);
     tween3.chain(tween4);
-    tween4.chain(tween1);
+    tween4.chain(tween1, tween1r);
 
     function startStopAnimation() {
       if (running) {
         tween1.start();
+        tween1r.start();
       } else {
         tween1.stop();
+        tween1r.stop();
       }
       animate();
     }
