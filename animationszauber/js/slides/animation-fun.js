04e91af7892f2c32986530a0c69020ce7186dd41
nitram509 <maki@bitkings.de>
2014-03-29 22:40:17 +0100

    improved Mr. T animation

--- animationszauber/js/slides/animation-fun.js
+++ animationszauber/js/slides/animation-fun.js
@@ -2,32 +2,42 @@
 // require jQuery.js
 
 (function () {
+
   var running = false;
+  var bedcon = $('#we-need-you');
+  var up = false;
+  var startDelay = 3000;
+
+  function auto_toggle_mr_t(e) {
+    if (!running) return;
+    if (bedcon.hasClass("active2")) {
+      bedcon.removeClass("active2");
+    } else {
+      bedcon.addClass("active2");
+    }
+    up = !up;
+    if (up) {
+      setTimeout(auto_toggle_mr_t, 500);
+    } else {
+      setTimeout(auto_toggle_mr_t, 3000);
+    }
+  }
+
+  function bringToFront() {
+    bedcon.addClass("active1");
+  }
 
   var slideChangeHandler = {
     onEnter: function (event) {
       running = true;
-      var up = false;
-
-      function auto_toggle_mr_t(e) {
-        if (!running) return;
-        var bedcon = $('#we-need-you');
-        if (bedcon.hasClass("active")) {
-          bedcon.removeClass("active");
-        } else {
-          bedcon.addClass("active");
-        }
-        up = !up;
-        if (up) {
-          setTimeout(auto_toggle_mr_t, 500);
-        } else {
-          setTimeout(auto_toggle_mr_t, 3000);
-        }
+      if (running) {
+        setTimeout(bringToFront, startDelay);
+        setTimeout(auto_toggle_mr_t, startDelay*2);
       }
-
-      if (running) setTimeout(auto_toggle_mr_t, 3000);
     },
     onLeave: function (event) {
+      bedcon.removeClass("active1");
+      bedcon.removeClass("active2");
       running = false;
     }
   };
